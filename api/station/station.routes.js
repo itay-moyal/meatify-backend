6153c349668c1288f5d9781f0e1f92a60648a537
Itay <itaymoyal31@gmail.com>
2026-07-10 22:41:10 +0300

    Refactor station routes to include removeStation functionality and improve comments

--- api/station/station.routes.js
+++ api/station/station.routes.js
@@ -3,13 +3,14 @@ import express from "express"
 import { log } from "../../middlewares/logger.middleware.js"
 
 //prettier-ignore
-import { getStations, getStationById, saveStation,removeSong,addSong } from "./station.controller.js"
+import { getStations, getStationById, saveStation,removeStation,removeSong,addSong } from "./station.controller.js"
 
 export const stationRoutes = express.Router()
 
 stationRoutes.get("/", log, getStations)
 stationRoutes.get("/:id", log, getStationById)
-stationRoutes.post("/", saveStation) // Add
-stationRoutes.put("/:id", saveStation) // Update
+stationRoutes.post("/", saveStation) // Add Station
+stationRoutes.put("/:id", saveStation) // Update Station
+stationRoutes.delete("/:id", removeStation) // Remove Station
 stationRoutes.put("/:id/song", addSong) // Add song
 stationRoutes.delete("/:id/song/:songId", removeSong) // Delete song
